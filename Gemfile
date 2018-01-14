fbc1d43594bb7d0b578e5e6a1eb364ddf9013af4
zhangjingqiang <zhangjingqiang@gmail.com>
2018-01-14 11:27:17 +0000

    Add uniq check to book && Update pg version

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ group :development, :test do
 end
 
 group :production do
-  gem 'pg'
+  gem 'pg', '~> 0.20'
   gem 'rails_12factor'
 end
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,3 @@
 class Book < ActiveRecord::Base
-  validates :name, presence: true, length: { maximum: 255 }
+  validates :name, presence: true, uniqueness: true,length: { maximum: 255 }
 end
